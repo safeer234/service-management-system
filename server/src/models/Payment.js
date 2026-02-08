ae0595a777edfe9608e971cbafe82888d2a87af1
safeer234 <sfrsmk268@gmail.com>
2026-02-08 18:09:53 +0530

    added payment model

---
+++ server/src/models/Payment.js
@@ -0,0 +1,19 @@
+import mongoose, { Types } from "mongoose";
+
+
+const paymentSchema = new mongoose.Schema({
+serviceRequest:{type:mongoose.Schema.Types.ObjectId,ref:"ServiceRequests",required:true},
+client:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+provider:{type:mongoose.Schema.Types.ObjectId,ref:"User", required:true},
+amount:{type:Number, required:true},
+paymentMethod:{type:String,enum:["cash","upi","card"],required:true},
+status:{type:String,enum:["pending","paid"],default:"pending"},
+paidAt:{type:Date}
+
+},
+{
+    timestamps:true
+}
+)
+
+const Payment = mongoose.model("Payment",paymentSchema)
