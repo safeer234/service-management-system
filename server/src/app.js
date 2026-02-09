9cf804ebe1d6a570a2d8e428978bea8728f38de1
safeer234 <sfrsmk268@gmail.com>
2026-02-09 10:21:15 +0530

    added routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import adminRoutes from "./routes/adminRoutes"
+import authRoutes from "./routes/authRoutes"
+import clientRoutes from "./routes/clientRoutes"
+import paymentRoutes from "./routes/paymentRoutes"
+import providerRoutes from "./routes/providerRoutes"
 const app = express();
 
 
 app.use(express.json());
+
+
+app.use("/admin",adminRoutes);
+app.use("/auth",authRoutes);
+app.use("/client",clientRoutes);
+app.use("/payment",paymentRoutes);
+app.use("/provider",providerRoutes);
 
 export default app;
