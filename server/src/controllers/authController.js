74de3618f868f88e1dccc96901706cd787f40ec8
safeer234 <sfrsmk268@gmail.com>
2026-03-01 17:14:29 +0530

    fixed server

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils/generateToken.js";
-import Provider from "../models/providerModel.js";
+import Provider from "../models/ProviderProfile.js";
 
 export const signup  = async(req,res) =>{
    try {
