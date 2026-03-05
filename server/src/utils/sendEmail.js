94b2433db94030fdca3b1a6ab0732bf41b57e953
safeer234 <sfrsmk268@gmail.com>
2026-03-05 11:55:09 +0530

    fixed server

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,16 +1,17 @@
+import dotenv from "dotenv";
 import { Resend } from "resend";
+
+dotenv.config(); // load env first
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ email, subject, message }) => {
-
   await resend.emails.send({
     from: "ServiceHub <onboarding@resend.dev>",
     to: email,
     subject: subject,
     text: message
   });
-
 };
 
 export default sendEmail;
