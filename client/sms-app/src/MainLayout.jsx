a647789629a972afc94138d1eaf821c1fdf2790c
safeer234 <sfrsmk268@gmail.com>
2026-03-06 19:16:16 +0530

    fixed client

--- client/sms-app/src/MainLayout.jsx
+++ client/sms-app/src/MainLayout.jsx
@@ -4,14 +4,22 @@ import Navbar from "./client/components/common/Navbar";
 import Footer from "./client/components/common/Footer";
 import { Outlet } from "react-router-dom";
 import Chatbot from "./client/components/common/Chatbot";
+import { useSelector } from 'react-redux'
 function MainLayout() {
+   const isAuthenticated = useSelector((state)=>state.auth.isAuthenticated)
+  
   return (
     <>
       <Navbar />
       <Outlet />
     
       <Footer />
-       <Chatbot />
+      {!isAuthenticated ? (
+        <h1></h1>
+      ):(
+          <Chatbot />
+      )}
+     
     </>
   );
 }
